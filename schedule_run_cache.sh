7497133d15c296abba3cc85d53ff113d4803b469
Jacob Schaupp <jacob.schaupp@gmail.com>
2026-01-16 08:33:55 -0500

    Remove schedule scripts

--- schedule_run_cache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --time=01:00:00
-#SBATCH --output=logs/%x-%j.out
-
-cd /data/js400421/volume-sim
-./run_cache.sh
--- schedule_run_cache_native.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --time=01:00:00
-#SBATCH --output=logs/volume-sim/%x-%j.out
-
-cd /data/js400421/volume-sim
-./run_cache_native.sh
